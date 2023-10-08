cc9223b7fb580190c3123ead78e36c484889a8be
NurulWahdania <nurulwahdania182@gmail.com>
2023-10-08 17:03:24 +0800

    Upload task 4

--- H071231005/Prartice Task 4/assigment4.num1.py
+++ H071231005/Prartice Task 4/assigment4.num1.py
@@ -1,7 +1,7 @@
 def mutasi(kata):
     try:
         for i in range(len(kata)):
-            kata = kata[-1] + kata[:-1] # [1:]+[0]
+            kata = kata[-1] + kata[:-1]
             print (kata, end=' | ')
     except TypeError:
         print ('error')
