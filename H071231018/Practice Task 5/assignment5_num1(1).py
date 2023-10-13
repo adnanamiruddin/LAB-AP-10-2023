c875c2a759da2533a03cdf0058256690ad299add
fadhilahmeisya <fadhilahmazzahrah@gmail.com>
2023-10-13 10:01:59 +0800

    Upload Task 5

--- H071231018/Practice Task 5/assignment5_num1(1).py
+++
@@ -1,5 +0,0 @@
-def campur(s1, s2):
-    hasil = s1[0] + s2[2] + s1[1] + s2[1] + s1[2] + s2[0]
-    print("Hasil mixed = " ,hasil)
-
-campur("Abc","Xyz") 
--- H071231018/Practice Task 5/assignment5_num1.py
+++ H071231018/Practice Task 5/assignment5_num1.py
@@ -14,15 +14,3 @@ for i in range(lenMax):
         hasil += s2[i]
         
 print(f"Hasil mixed: {hasil}")
-
-# s1 = input("s1: ")
-# s2 = input("s2: ")
-# hasil = ""
-
-# minim=min((s1),(s2))
-
-# for i in range(len(minim)):
-#     balik="".join(reversed(s2))
-#     hasil += s1[i] + balik[i]
-
-# print(hasil)
